0038b7de5a7e131b10ac30dd2b780f6b470b6320
sharpTrick <psharp.x13@gmail.coom>
2015-04-17 20:39:30 -0600

    Added edit URLs

--- platty/urls.py
+++ platty/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^create/$', views.create, name='create'),
+    url(r'^edit/(?P<event_id>[0-9]+)$', views.edit, name='edit'),
     url(r'^find/$', views.find, name='find'),
     url(r'^parties/$', views.parties, name='parties'),
     url(r'^login/$', views.login, name='login'),
