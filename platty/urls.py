e808a3d92106ab554e0edba0d93f8ac85af9375d
corrosiveelement <corrosive.element@gmail.com>
2015-04-18 12:45:06 -0600

    Everything
    I just did almost everything. Most things should work now.

--- platty/urls.py
+++ platty/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     url(r'^create/$', views.create, name='create'),
     url(r'^find/$', views.find, name='find'),
     url(r'^parties/$', views.parties, name='parties'),
-    url(r'^login/$', views.login, name='login'),
+    url(r'^parties/(\d+)/$', views.party, name='parties'),
+    url(r'^login/$', views.login_page, name='login'),
+    url(r'^logout/$', views.logout_page, name='logout'),
     url(r'^profile/$', views.profile, name='profile'), 
 ]
--- platty/views.py
+++ platty/views.py
@@ -1,37 +1,117 @@
 from django.http import HttpResponse
-from django.shortcuts import render, render_to_response, get_object_or_404
+from django.shortcuts import render, render_to_response, get_object_or_404, redirect
 from django.template import RequestContext, loader
+from django.contrib.auth import authenticate, login, logout
 
 from django.db import models
 from datetime import datetime
-from .models import Event
+from .models import *
 
 def index(request):
         return HttpResponse('Hey there, you are at the home index!')
 # Create your views here.
 
 def home(request):
-    return render(request, 'platty/index.html', {})
+    if request.user.is_active:
+        template = loader.get_template('platty/index.html')
+        context = RequestContext(request, {
+            'selected_page': 'home',
+            'context_instance': request
+        })
+        return HttpResponse(template.render(context))
+    else:
+        return redirect('/login/')
 
 def create(request):
-    p = get_object_or_404(Event, pk=event_id)
-    event = Event(date_time = datetime.now(), name="fdasfs", location="fdsfa", description="fdsafdsa")
-    event_list = Event.objects.order_by('-id')[:5]
-    template = loader.get_template('create.html')
-    context = RequestContext(request, {
-        'event_list': event_list,
-    })
-    #event.save()
-    return HttpResponse(template.render(context))
+    if request.user.is_active:
+        if 'submit' not in request.POST:
+            template = loader.get_template('platty/create.html')
+            context = RequestContext(request, {
+                'context_instance': request
+            })
+            return HttpResponse(template.render(context))
+        else:
+            new_event = Event(
+                name=request.POST['name'],
+                description=request.POST['description'],
+                date_time=request.POST['date'] + ' ' + request.POST['time'],
+                addressLineOne=request.POST['address1'],
+                addressLineTwo=request.POST['address2'],
+                city=request.POST['city'],
+                state=request.POST['state'],
+                zipCode=request.POST['zip']
+            )
+            new_event.save()
+            role = Role(
+                user = request.user,
+                event = new_event,
+                role=0
+            )
+            role.save()
+            return redirect('/parties/')                
+    else:
+        return redirect('/login/')
 
 def parties(request):
+    if request.user.is_active:
+        hosting_list = Event.objects.filter(role__user=request.user.id, role__role=0).order_by('-name')
+        going_list = Event.objects.filter(role__user=request.user.id, role__role=1).order_by('-name')
+        template = loader.get_template('platty/parties.html')
+        context = RequestContext(request, {
+            'hosting_list': hosting_list,
+            'going_list': going_list,
+            'context_instance': request
+        })
+        return HttpResponse(template.render(context))
+    else:
+        return redirect('/login/')
+
+def party(request, party_id):
     return render_to_response('platty/parties.html', context_instance=RequestContext(request))
 
 def find(request):
-    return render_to_response('platty/find.html', context_instance=RequestContext(request))
+    if request.user.is_active:
+        template = loader.get_template('platty/find.html')
 
-def login(request):
+        searchedFor = request.GET.get('keywords', '')
+        event_list = Event.objects.filter(name__icontains=searchedFor)
+
+        context = RequestContext(request, {
+            'event_list': event_list,
+            'searched_for': searchedFor,
+            'context_instance': request
+        })
+
+        return HttpResponse(template.render(context))
+    else:
+        return redirect('/login/')
+
+def login_page(request):
+    if 'username' not in request.POST:
+	    return render_to_response('platty/login.html', context_instance=RequestContext(request))
+
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return redirect('/')
+            # Redirect to a success page.
+        else:
+            x = 5
+            # Return a 'disabled account' error message
+    else:
+        x = 5
+        # Return an 'invalid login' error message.
     return render_to_response('platty/login.html', context_instance=RequestContext(request))
+    
+def logout_page(request):
+    logout(request)
+    return redirect('/login/')
 
 def profile(request):
-    return render_to_response('platty/profile.html', context_instance=RequestContext(request))
+    if request.user.is_active:
+        return render_to_response('platty/profile.html', context_instance=RequestContext(request))
+    else:
+        return redirect('/login/')
