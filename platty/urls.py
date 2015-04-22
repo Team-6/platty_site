e5d9fe99c79568fdbbd51ae090dec25eb5ece1f6
sharpTrick <psharp.x13@gmail.coom>
2015-04-22 13:57:07 -0600

    Requirements are Live!!!

--- platty/urls.py
+++ platty/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     url(r'^parties/$', views.parties, name='parties'),
     url(r'^parties/(\d+)/$', views.party, name='parties'),
     url(r'^edit/(?P<party_id>[0-9]+)/$', views.edit, name='edit'),
+    url(r'^edit/(?P<party_id>[0-9]+)/submit/$', views.submit, name='submit'),
     url(r'^login/$', views.login_page, name='login'),
     url(r'^logout/$', views.logout_page, name='logout'),
     url(r'^profile/$', views.profile, name='profile'), 
