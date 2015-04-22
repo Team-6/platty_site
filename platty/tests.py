9ce465ebe3882553c9f484f57bf36e9da79a4b3c
eric <meissner.eric.7@gmail.com>
2015-04-22 12:42:12 -0600

    Trivial test case example.

--- platty/tests.py
+++ platty/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from datetime import datetime
+from platty.models import *
 
 # Create your tests here.
+class EventTestCase(TestCase):
+    def setUp(self):
+        Event.objects.create(name="party1", date_time=datetime.now(), zipCode=84352)
+        Event.objects.create(name="party2", date_time=datetime.now(), zipCode=84334)
+    
+    def test_event(self):
+        p1 = Event.objects.get(name="party1")
+        p2 = Event.objects.get(name="party2")
+
+        self.assertEqual(p1.zipCode, 84352)
+        self.assertEqual(p2.zipCode, 84334)
