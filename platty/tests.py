8a97c7f962ccb58ca76fec7b091f1a4146ad3323
eric <meissner.eric.7@gmail.com>
2015-04-24 13:58:01 -0600

    Tests examples.

--- platty/tests.py
+++ platty/tests.py
@@ -9,7 +9,23 @@ class EventTestCase(TestCase):
     def setUp(self):
         Event.objects.create(name="party1", date_time=datetime.now(), zipCode=84352)
         Event.objects.create(name="party2", date_time=datetime.now(), zipCode=84334)
-    
+        self.c = Client()
+        
+        self.username = "muffin"
+        self.password = "muffinman"
+        self.email = "muffinman@gmail.com"
+        User.objects.create_user(
+            self.username,
+            self.email,
+            self.password,
+            first_name = "Eric",
+            last_name = "Muffin",
+        )
+
+        response = self.c.post('/login/', {
+            'username' : "muffin",
+            'password' : "muffinman",})
+
     def test_event(self):
         p1 = Event.objects.get(name="party1")
         p2 = Event.objects.get(name="party2")
@@ -18,55 +34,27 @@ class EventTestCase(TestCase):
         self.assertEqual(p2.zipCode, 84334)
 
     def test_signup(self):
-        c = Client()
 
         #submitButtonClicked information given, redirected to login page.
-        response = c.post('/signup/', { 'submit' : 'submit',
-                                        'username' : 'muffin', 
+        response = self.c.post('/signup/', { 'submit' : 'submit',
+                                        'username' : 'muffinman', 
                                         'email' : 'myemail@email.com',
                                         'password' : 'password',
                                         'firstName' : 'Eric',
                                         'lastName' : 'Muffin', }, follow=True )
-        
-        user = User.objects.get(email="myemail@email.com")
         self.assertEqual(len(response.redirect_chain), 1)
-        self.assertEqual(user.email, "myemail@email.com")
         
-        response = c.post('/signup/', { 
-                                        'username' : 'muffin', 
-                                        'email' : 'myemail@email.com',
-                                        'password' : 'password',
-                                        'firstName' : 'Eric',
-                                        'lastName' : 'Muffin', }, follow=True )
-        
-        #same. submit button not clicked
-        self.assertEqual(len(response.redirect_chain), 0)
-
     def test_event_create(self):
         
         #todo how do I set user to be active?
-
-        c = Client()
-
         
         #create and login the user
-        username = "muffin"
-        password = "themuffinman"
-        email = "muffinman@gmail.com"
-        User.objects.create_user(
-            username,
-            email,
-            password,
-            first_name = "Eric",
-            last_name = "Muffin",
-        )
 
-        user = authenticate(username=username, password=password)
+        user = authenticate(username=self.username, password=self.password)
         user.is_active = True
 
         #make a create request with valid information. clicked the button
-        response = c.post('/create/', { 'user' : user,
-                                        'verified' : 'yes',
+        response = self.c.post('/create/', { 'verified' : 'yes',
                                         'submit' : 'submit',
                                         'name' : 'The Muffin Man', 
                                         'description' : 'The Muffin Man?',
@@ -80,6 +68,6 @@ class EventTestCase(TestCase):
         muffinParty = Event.objects.get(name="The Muffin Man")
         
         self.assertEqual(len(response.redirect_chain), 1)
-        self.assertEqual(response.redirect_chain[0], "http://localhost::8000/parties/")
+        self.assertEqual(response.redirect_chain[0][0], "http://testserver/parties/")
         self.assertEqual(muffinParty.name, "The Muffin Man")
 
